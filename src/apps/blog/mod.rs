886dd1cfbb71739c5acb11a578bc613b622b0c99
madeinheaven91 <exspectanss@gmail.com>
2025-03-01 14:43:00 +0300

    fix

--- src/apps/blog/mod.rs
+++ src/apps/blog/mod.rs
@@ -13,14 +13,16 @@ mod messages;
 pub fn service() -> Scope {
     let protected = CONFIG.environment.auth_enabled();
 
-    web::scope("/users")
-        .route("", web::get().to(handlers::get_posts))
-        .route("", web::post().to(handlers::create_post))
-        .route("/{id}", web::get().to(handlers::get_post))
-        .service(
-            web::scope("")
-                .wrap(Condition::new(protected, from_fn(auth_middleware)))
-                .route("/{id}", web::patch().to(handlers::update_post))
-                .route("/{id}", web::delete().to(handlers::delete_post)),
-        )
+    web::scope("/blog").service(
+        web::scope("/posts")
+            .route("", web::get().to(handlers::get_posts))
+            .route("", web::post().to(handlers::create_post))
+            .route("/{slug}", web::get().to(handlers::get_post))
+            .service(
+                web::scope("")
+                    .wrap(Condition::new(protected, from_fn(auth_middleware)))
+                    .route("/{slug}", web::patch().to(handlers::update_post))
+                    .route("/{slug}", web::delete().to(handlers::delete_post)),
+            ),
+    )
 }
