e082ea07c15e98c270dc29808c1c01a1eecace62
madeinheaven91 <exspectanss@gmail.com>
2025-03-12 00:45:48 +0300

    dockerized

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -3,12 +3,13 @@ import HomeView from "./views/HomeView.vue";
 import PostView from "./views/PostView.vue";
 import EditorView from "./views/EditorView.vue";
 import DraftsView from "./views/DraftsView.vue";
+import NotFoundView from "./views/NotFoundView.vue";
 
 const routes = [
+  { path: "/", redirect: "/blog", },
   { path: "/blog", component: HomeView, },
   { path: "/blog/new", component: EditorView, },
   { path: "/blog/drafts", component: DraftsView, },
-  { path: "/", redirect: "/blog", },
   {
     path: "/blog/post/:slug",
     component: PostView,
@@ -19,6 +20,7 @@ const routes = [
     component: EditorView,
     props: true
   },
+  { path: '/:pathMatch(.*)*', component: NotFoundView, }
 ];
 
 const router = createRouter({
---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="mt-3 text-secondary">Страница не найдена...</h1>
+</template>
